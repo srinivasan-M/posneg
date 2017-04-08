589714f7b2552ebba6ee8c1434066277e2a2bde5
srinivasan-M <srinivasanksr15@gmail.com>
2017-04-08 11:04:34 +0530

    Update posneg.java

--- posneg.java
+++ posneg.java
@@ -3,4 +3,18 @@ class posneg
 {
 public static void main(String args[])
 {
-
+int n=Integer.parseInt(args[0]);
+if(n>0)
+{
+  System.out.println("Number is positive");
+}
+  else if(n<0)
+  {
+    System.out.println("Number is negative");
+  }
+  else
+  {
+    System.out.println("Number is zero");
+  }
+}
+}
